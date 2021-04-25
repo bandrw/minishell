0b8317986f4a029b98d16e0ff6dc3aced6865fa6
pantigon <pantigon@student.21-school.ru>
2021-04-25 19:56:03 +0300

    add pwd, other

--- srcs/parse/parse.c
+++ srcs/parse/parse.c
@@ -12,18 +12,40 @@
 
 #include "minishell.h"
 
+void 	ft_get_echo(t_parse *parse, char *str)
+{
+
+}
+
+void	ft_get_pwd(t_parse *parse)
+{
+	parse->command_id = CMD_PWD;
+	parse->argv = (char **)malloc(sizeof(char*) * 2);
+//		if (!parse->argv)
+	parse->argv[0] = ft_strdup("pwd");
+	parse->argv[1] = 0;
+}
+
+void	ft_get_other(t_parse *parse, char *str)
+{
+	parse->command_id = CMD_OTHER;
+	parse->argv = ft_split(str, ' ');
+}
+
 void	parse_line(char *command_line, t_parse *parse)
 {
-    if (ft_strncmp(command_line[0]))
-//	// echo example
+
+	if (ft_strncmp("echo", command_line, 5))
+		ft_get_echo(parse, command_line);
+	if (ft_strncmp("pwd", command_line, 4))
+		ft_get_pwd(parse);
+	else
+		ft_get_other(parse, command_line);
+		//	// echo example
 //	parse->command_id = CMD_ECHO;
 //	parse->argv = (char **)malloc(sizeof(char *) * 4);
 //	parse->argv[0] = ft_strdup("abcd");
 //	parse->argv[1] = ft_strdup("123");
 //	parse->argv[2] = ft_strdup("456");
 //	parse->argv[3] = 0;
-
-	// other commands example
-	parse->command_id = CMD_OTHER;
-	parse->argv = ft_split(command_line, ' ');
 }
