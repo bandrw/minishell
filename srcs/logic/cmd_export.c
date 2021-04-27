ba0209bce6a6a91577cc2da668095343c1c2b093
kfriese <kfriese@student.21-school.ru>
2021-04-27 20:41:39 +0300

    export no args added

--- srcs/logic/cmd_export.c
+++ srcs/logic/cmd_export.c
@@ -12,17 +12,34 @@
 
 #include "minishell.h"
 
-void	ft_export(t_list *argv, char **env)
+static void	write_vars(char **env)
 {
-	int		i;
-	char	**pair;
+	int	i;
 
 	i = -1;
-	while (argv)
+	while (env[++i])
 	{
-		pair = ft_split(argv->content, '=');
-		
-		printf("Export: %s will be %s\n", pair[0], pair[1]);
-		argv = argv->next;
+		ft_putstr("declare -x ");
+		ft_putendl_fd(env[i], 1);
+	}
+}
+
+void	ft_export(t_list *argv, char **env)
+{
+	char	**pair;
+
+	if (!argv)
+		write_vars(env);
+	else
+	{
+		while (argv)
+		{
+			pair = ft_split(argv->content, '=');
+			if (pair[1])
+				printf("Export: %s will be %s\n", pair[0], pair[1]);
+			else
+				printf("Export: %s (null)\n", pair[0]);
+			argv = argv->next;
+		}
 	}
 }
